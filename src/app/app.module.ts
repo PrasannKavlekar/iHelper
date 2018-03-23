1be2a22d5b0f11ca3ea9a8aefef14dfb8f411186
Prasann Kavlekar <prasannkavlekar9@gmail.com>
2018-03-23 11:16:37 +0530

    v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { WelcomePage } from '../pages/welcome/welcome'
+import { LoginPage } from '../pages/login/login'
+import { SignupPage } from '../pages/signup/signup'
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ProfilePage } from '../pages/profile/profile';
+import { CartPage } from '../pages/cart/cart';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    WelcomePage,
+    LoginPage,
+    SignupPage,
     HomePage,
+    CartPage,
+    ProfilePage,
     TabsPage
   ],
   imports: [
@@ -26,15 +32,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    WelcomePage,
+    LoginPage,
+    SignupPage,
     HomePage,
+    CartPage,
+    ProfilePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
